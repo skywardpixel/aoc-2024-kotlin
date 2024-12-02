e8989f5a54a3a2a69cffad2d57418ee9bef850fc
Kyle Yan <kyleyankc@gmail.com>
2024-12-02 05:55:50 -0800

    Add Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,34 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (left, right) = input
+            .map { line ->
+                val left = line.substringBefore(" ")
+                val right = line.substringAfterLast(" ")
+                left.toInt() to right.toInt()
+            }
+            .unzip()
+        return left.sorted().zip(right.sorted())
+            .sumOf { (left, right) -> (left - right).absoluteValue }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (left, right) = input
+            .map { line ->
+                val left = line.substringBefore(" ")
+                val right = line.substringAfterLast(" ")
+                left.toInt() to right.toInt()
+            }
+            .unzip()
+        val rightCounts = right.groupingBy { it }.eachCount()
+        return left.sumOf { it * (rightCounts[it] ?: 0) }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
